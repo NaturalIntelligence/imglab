47c298bfca9f896bc262baf308c5b299925acf06
Amit Gupta <amitgupta.gwl@gmail.com>
2017-10-25 15:15:58 +0530

    Initial working commit

---
+++ js/Face++DataParser.js
@@ -0,0 +1,102 @@
+
+
+var alteredData;
+var actualData;
+function fetch(){
+
+	var formData = new FormData();
+	formData.append('api_key', 'yourkey');
+	formData.append('api_secret', 'yoursecret');
+	formData.append('return_landmark', 1);
+	//formData.append('return_attributes', "gender,age,smiling,headpose,facequality,blur,eyestatus,ethnicity,emotion");
+	formData.append('return_attributes', "gender,age,headpose");
+	if($("#image_url").val() != ""){
+		formData.append('image_url', $("#image_url").val());
+	}else{
+		formData.append('image_file', $('input[type=file]')[0].files[0]); 
+	}
+	// Attach file
+
+	$.ajax({
+	    url: 'https://api-us.faceplusplus.com/facepp/v3/detect',
+	    data: formData,
+	    type: 'POST',
+	    contentType: false, // NEEDED, DON'T OMIT THIS (requires jQuery 1.6+)
+	    processData: false, // NEEDED, DON'T OMIT THIS
+	    success: function(data) {
+            //jsonData = $.parseJSON(data);
+            actualData = data;
+            alteredData = data;
+            $('#actualData').text(JSON.stringify(data));
+            $('#alteredData').text(JSON.stringify(data));
+            deleteAllPoints();
+            plotWith(data);
+	    },
+	    error: function(err) {
+	    	$("#info").text("Error in connecting face++ API. Check console for more deetail");
+	    	console.error("Error in connecting face++ API", err);
+	    }
+	});
+}
+
+var deleteAllPoints= function(){
+	$("#img_overlay").empty();
+}
+
+function plotWith(jsonData){
+	if(jsonData.faces.length > 0){
+		jsonData.faces.forEach(function(face,index){
+			drawRectangle(face.face_rectangle,face.attributes.gender.value);
+			drawPoints(index,face.landmark);
+			//jsPlumb.draggable($(".ptn"));
+		});
+		$("#info").text(jsonData.faces.length + " face(s) detected!!");
+	}else{
+		$("#info").text("No face detected!!");
+	}
+}
+
+var drawPoints = function(id,landmark){
+	labels.forEach(function(property,index){
+		if(property !== "other")
+		drawPoint(id,landmark[property],index);	
+	});
+	
+}
+
+function drawPoint(id,coordinates,num){
+  var point = getPointElement(id,coordinates.x,coordinates.y,labels[num]);
+  $("#img_overlay").append(point);
+  jsPlumb.draggable(point,{
+  	/*drag: function(e){
+        //console.log(e.pos[0]); // for left position
+    	//console.log(e.pos[1]); // for top position
+    },*/
+    stop: function(e){
+        var lbl = $(e.el).attr("label");
+        if(labels.indexOf(lbl)){
+        	var tmp = e.el.id.split("_");
+        	console.log(alteredData.faces[tmp[0]].landmark[lbl]);
+        	alteredData.faces[tmp[0]].landmark[lbl].x = e.pos[0];
+        	alteredData.faces[tmp[0]].landmark[lbl].y = e.pos[1];
+        }
+        //console.log(e.pos[0]); // for left position
+    	//console.log(e.pos[1]); // for top position
+    }
+  });
+}
+
+function getPointElement(id,x,y,lbl){
+	return $('<div class="ptn" id="'+id+'_'+ lbl+'" label="'+lbl+'"></div>')
+            .css('top', (y - 3) + 'px')
+            .css('left', (x - 3) + 'px');
+}
+
+function clearCanvas(canvas) {
+	
+    var ctx = canvas.getContext('2d');
+    var width = canvas.width,
+        height = canvas.height;
+    ctx.fillStyle = '#EEE';
+    ctx.fillRect(0, 0, width, height);
+}
---
+++ js/FaceBox.js
@@ -0,0 +1,49 @@
+var drawRectangle = function(face_rectangle,gender){
+	// change box color based on gender
+	var rgbColor,
+        rgbaColor;
+
+    if (gender === 'Male') {
+        rgbColor = '#12BDDC';
+        rgbaColor = 'rgba(18,189,220,0.8)';
+    } else {
+        rgbColor = '#C537D8';
+        rgbaColor = 'rgba(197,55,216,0.8)';
+    }
+
+    //var canvas = $('#canvas');
+    //clearCanvas(canvas);
+
+    $('<div/>').css({
+        position: 'absolute',
+        top: face_rectangle.top - 5,
+        left: face_rectangle.left - 5,
+        width: face_rectangle.width + 5,
+        height: face_rectangle.height + 5,
+        border: '3px solid ' + rgbColor,
+        borderColor: rgbaColor,
+        borderRadius: '10px'
+    }).
+    /*qtip({
+        content: '<table>' +
+                     '<tr><td>width</td><td>'        + (face.width * 0.01).toFixed(2) + '</td></tr>' +
+                     '<tr><td>height</td><td>'       + (face.height * 0.01).toFixed(2) + '</td></tr>' +
+                     '<tr><td>center</td><td>('      + (face.center.x      * 0.01).toFixed(2) + ', ' + (face.center.y      * 0.01).toFixed(2) + ')</td></tr>' +
+                     '<tr><td>eye_left</td><td>('    + (face.eye_left.x    * 0.01).toFixed(2) + ', ' + (face.eye_left.y    * 0.01).toFixed(2) + ')</td></tr>' +
+                     '<tr><td>eye_right</td><td>('   + (face.eye_right.x   * 0.01).toFixed(2) + ', ' + (face.eye_right.y   * 0.01).toFixed(2) + ')</td></tr>' +
+                     '<tr><td>mouth_left</td><td>('  + (face.mouth_left.x  * 0.01).toFixed(2) + ', ' + (face.mouth_left.y  * 0.01).toFixed(2) + ')</td></tr>' +
+                     '<tr><td>mouth_right</td><td>(' + (face.mouth_right.x * 0.01).toFixed(2) + ', ' + (face.mouth_right.y * 0.01).toFixed(2) + ')</td></tr>' +
+                     '<tr><td>race</td><td>'         + face.attribute.race.value + ' (' + face.attribute.race.confidence.toFixed(2) + '%)</td></tr>' +
+                     '<tr><td>age</td><td>'          + face.attribute.age.value + ' (&#177;' + face.attribute.age.range + ')</td></tr>' +
+                     '<tr><td>gender</td><td>'       + face.attribute.gender.value + ' (' + face.attribute.gender.confidence.toFixed(2) + '%)</td></tr>' +
+                 '</table>',
+        style: {
+            classes: 'detector-tooltip ui-tooltip-light ui-tooltip-tipify'
+        },
+        position: {
+            my: 'bottom center',
+            at: 'top center'
+        }
+    }).*/
+    appendTo($("#img_home"));	
+}
