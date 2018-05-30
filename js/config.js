eda2b1449f19da2bbc389eddd9f9e384bceef1b4
Amit Gupta <amitgupta.gwl@gmail.com>
2018-05-30 15:14:12 +0530

    use svg.js
    - create rectangle on tool select and mouse event
    - drag rectangle

--- js/config.js
+++ js/config.js
@@ -1,43 +1,46 @@
 var tools = {
-    labelling : [
-        {
-            id : "tool-point" ,
+    labelling : {
+        "tool-point" : {
             title  : "Point",
             desp : "Create a feature point inside the concave polygon or boundary box",
             icon : "point.svg"
-        },{
-            id : "tool-rectangle" ,
+        },
+        "tool-rectangle" : {
             title  : "Rectangle",
             desp : "Create a Boundary boxrectangle",
-            icon : "rectangle.svg"
-        },{
-            id : "tool-polygon" ,
+            icon : "rectangle.svg",
+            create : function(){
+                var rect =  myCanvas.rect().addClass('labelbox').draw();
+                rect.draggable();
+                return rect;
+            }
+        },
+        "tool-polygon" : {
             title  : "Polygon",
             desp : "Create a concave polygon",
             icon : "polygon.svg"
         }
-    ],
-    canvas : [
-        {
-            id : "tool-move" ,
+    },
+    canvas : {
+        "tool-move" : {
             title  : "Move",
             desp : "Move an element or the entire workarea",
             icon : "move.svg"
-        },{
-            id : "tool-zoom-in" ,
+        },
+        "tool-zoom-in" : {
             title  : "Zoom In",
             desp : "Enlarge the workarea",
             icon : "zoomin.svg"
-        },{
-            id : "tool-zoom-out" ,
+        },
+        "tool-zoom-out" : {
             title  : "Zoom Out",
             desp : "Create a concave polygon",
             icon : "zoomout.svg"
-        },{
-            id : "tool-labels-only" ,
+        },
+        "tool-labels-only" : {
             title  : "Labels only",
             desp : "Hide the image",
             icon : "lightbulb.svg"
         }
-    ]
+    }
 }
--- tags/tags.js
+++ tags/tags.js
@@ -95,18 +95,32 @@ riot.tag2('menu', '', '', '', function(opts) {
 });
 riot.tag2('statusbar', '', '', '', function(opts) {
 });
-riot.tag2('toolbox', '<div each="{tool in tools[opts.tools]}" id="{tool.id}" class="tool-button" onclick="{selectme}"> <img class="tool-icon" riot-src="img/icons/{tool.icon}"> <div>{tool.title}</div> </div>', 'toolbox .tool-button,[data-is="toolbox"] .tool-button{ text-align: center; margin: 14px 0px; font-size: 0.9em; } toolbox .tool-icon,[data-is="toolbox"] .tool-icon{ width: 40%; margin: 3px 0px; } toolbox .tool-selected,[data-is="toolbox"] .tool-selected{ background: coral; } toolbox .tool-button:not(.tool-selected):hover,[data-is="toolbox"] .tool-button:not(.tool-selected):hover{ background: grey; }', '', function(opts) {
+riot.tag2('toolbox', '<div each="{tool,toolid in tools[opts.tools]}" id="{toolid}" class="tool-button" onclick="{selectme}"> <img class="tool-icon" riot-src="img/icons/{tool.icon}"> <div>{tool.title}</div> </div>', 'toolbox .tool-button,[data-is="toolbox"] .tool-button{ text-align: center; margin: 14px 0px; font-size: 0.9em; } toolbox .tool-icon,[data-is="toolbox"] .tool-icon{ width: 40%; margin: 3px 0px; } toolbox .tool-selected,[data-is="toolbox"] .tool-selected{ background: coral; } toolbox .tool-button:not(.tool-selected):hover,[data-is="toolbox"] .tool-button:not(.tool-selected):hover{ background: grey; }', '', function(opts) {
 
         var tag = this;
-        tag.selectme = selectme;
 
-        function selectme(e){
+        tag.selectme = function (e){
 
             $(".tool-selected").removeClass("tool-selected");
             $(e.currentTarget).addClass("tool-selected");
+            selectedTool = tools[opts.tools][e.item.toolid];
 
         }
 });
 
-riot.tag2('workarea', '<div id="canvas-container"> <img id="img" riot-src="{opts.img.src}" width="{opts.img.size.width}" height="{opts.img.size.height}"> <canvas id="work-canvas" width="{opts.img.size.width}" height="{opts.img.size.height}"></canvas> <span id="tooltip-span"></span> <div id="v_line"></div> <div id="h_line"></div> </div>', 'workarea #work-canvas,[data-is="workarea"] #work-canvas{ position: absolute; z-index: 1; } workarea #canvas-container,[data-is="workarea"] #canvas-container{ height: calc(100vh - 150px); display: block; overflow: auto; position: relative; }', '', function(opts) {
+riot.tag2('workarea', '<div id="canvas-container"> <img id="img" riot-src="{opts.img.src}" width="{opts.img.size.width}" height="{opts.img.size.height}"> <div id="work-canvas" width="{opts.img.size.width}" height="{opts.img.size.height}"></div> <span id="tooltip-span"></span> <div id="v_line"></div> <div id="h_line"></div> </div>', 'workarea #work-canvas,[data-is="workarea"] #work-canvas{ position: absolute; z-index: 1; } workarea #canvas-container,[data-is="workarea"] #canvas-container{ height: calc(100vh - 150px); display: block; overflow: auto; position: relative; }', '', function(opts) {
+        this.on('mount',function() {
+            myCanvas = new SVG('work-canvas').size(opts.img.size.width, opts.img.size.height);
+
+            myCanvas.on('mousedown', function(event){
+                if(selectedTool){
+                    currentToolElement = selectedTool.create();
+                    currentToolElement.draw(event);
+                }
+            });
+            myCanvas.on('mouseup', function(event){
+                if(selectedTool && currentToolElement)   currentToolElement.draw(event);
+            });
+
+        } );
 });
