d727916f812e4d1fbacf66ae94169b6b22a3b0c4
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-06-20 06:25:33 -0500

    Issue 31 - Mouse tracking : incomplete

--- js/app.js
+++ js/app.js
@@ -4,5 +4,19 @@ function showSnackBar(msg) {
     $("#snackbar").addClass("show");
 
     // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ $("#snackbar").removeClass("show"); $("#snackbar").text("");}, 3000);
+    setTimeout(function () {
+        $("#snackbar").removeClass("show");
+        $("#snackbar").text("");
+    }, 3000);
+}
+
+//get mouse cordinates on image
+function getCordinates(event, element) {
+    var rect = element[0].getBoundingClientRect();
+    var x = event.pageX - rect.left;
+    var y = event.pageY - rect.top;
+    return {
+        x: x,
+        y: y
+    };
 }
