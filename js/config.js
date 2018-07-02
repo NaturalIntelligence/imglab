edba757b3ec96a7c6435801fc3e574515d44d234
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-07-02 03:36:49 -0500

    Fix for Issue 30: svg shape should not be draggable when point tool is selected

--- js/config.js
+++ js/config.js
@@ -1,65 +1,65 @@
 var tools = {
-    labelling : {
-        "tool-point" : {
+    labelling: {
+        "tool-point": {
             type: "point",
-            title  : "Point",
-            desp : "Create a feature point inside the concave polygon or boundary box",
-            icon : "point.svg",
-            drawable : true,
-            create : function(e,container){
+            title: "Point",
+            desp: "Create a feature point inside the concave polygon or boundary box",
+            icon: "point.svg",
+            drawable: true,
+            create: function (e, container) {
                 var canvasOffset = myCanvas.node.getBoundingClientRect();
-                return getPointToDraw(e,container,canvasOffset);
+                return getPointToDraw(e, container, canvasOffset);
             },
-            validate: function(el){
+            validate: function (el) {
                 return true;
             }
         },
-        "tool-circle" : {
+        "tool-circle": {
             type: "circle",
-            title  : "Circle",
-            desp : "Create a circle",
-            icon : "circle.svg",
-            drawable : true,
-            create : function(){
-                var circle =  myCanvas.nested().circle().radius().addClass('labelcircle shape')/* .draw() */;
+            title: "Circle",
+            desp: "Create a circle",
+            icon: "circle.svg",
+            drawable: true,
+            create: function () {
+                var circle = myCanvas.nested().circle().radius().addClass('labelcircle shape') /* .draw() */ ;
                 circle.resize();
                 circle.parent().draggable();
                 return circle;
             },
-            validate: function(el){
+            validate: function (el) {
                 return Number.parseInt(el.attr("r")) > 3;
             }
         },
-        "tool-rectangle" : {
+        "tool-rectangle": {
             type: "rect",
-            title  : "Rectangle",
-            desp : "Create a Boundary boxrectangle",
-            icon : "rectangle.svg",
-            drawable : true,
-            create : function(){
-                var rect =  myCanvas.nested().rect().addClass('labelbox shape')/* .draw() */;
+            title: "Rectangle",
+            desp: "Create a Boundary boxrectangle",
+            icon: "rectangle.svg",
+            drawable: true,
+            create: function () {
+                var rect = myCanvas.nested().rect().addClass('labelbox shape') /* .draw() */ ;
                 rect.resize();
                 rect.parent().draggable();
                 return rect;
             },
-            validate: function(el){
+            validate: function (el) {
                 return Number.parseInt(el.attr("width")) > 3;
             },
         },
-        "tool-polygon" : {
+        "tool-polygon": {
             type: "poly",
-            title  : "Polygon",
-            desp : "Create a concave polygon",
-            icon : "polygon.svg",
-            drawable : true,
-            create : function(){//TODO: bug: creating duplicate points
-                var poly =  myCanvas.nested().polygon().addClass('labelpolygon shape')/* .draw() */;
+            title: "Polygon",
+            desp: "Create a concave polygon",
+            icon: "polygon.svg",
+            drawable: true,
+            create: function () { //TODO: bug: creating duplicate points
+                var poly = myCanvas.nested().polygon().addClass('labelpolygon shape') /* .draw() */ ;
                 poly.resize();
                 poly.parent().draggable();
 
-                poly.on('drawstart', function(e){
-                    document.addEventListener('keydown', function(e){
-                        if(e.keyCode == 13){
+                poly.on('drawstart', function (e) {
+                    document.addEventListener('keydown', function (e) {
+                        if (e.keyCode == 13) {
                             poly.draw('done');
                             poly.off('drawstart');
                         }
@@ -68,53 +68,56 @@ var tools = {
 
                 return poly;
             },
-            validate: function(el){
+            validate: function (el) {
                 return true;
             },
         }
     },
-    canvas : {
-        "tool-move" : {
-            title  : "Move",
-            desp : "Move an element or the entire workarea",
-            icon : "move.svg",
-            type : "move",
+    canvas: {
+        "tool-move": {
+            title: "Move",
+            desp: "Move an element or the entire workarea",
+            icon: "move.svg",
+            type: "move",
         },
-        "tool-zoom" : {
-            title  : "Zoom In",
-            desp : "Enlarge the workarea",
-            icon_font : "icon-zoom-in",
+        "tool-zoom": {
+            title: "Zoom In",
+            desp: "Enlarge the workarea",
+            icon_font: "icon-zoom-in",
         },
-        "tool-labels-only" : {
-            title  : "Light",
-            desp : "Highlight the labels",
-            icon_font : "icon-lightbulb",
-            actions : ["lightbulb"]
+        "tool-labels-only": {
+            title: "Light",
+            desp: "Highlight the labels",
+            icon_font: "icon-lightbulb",
+            actions: ["lightbulb"]
         }
     }
 };
 
-function getPointToDraw(position,container,canvasOffset){
+function getPointToDraw(position, container, canvasOffset) {
     var containerOffset = {
         x: container.parent().attr("x"),
-        y :container.parent().attr("y")
+        y: container.parent().attr("y")
     }
-    var point =  container.parent().circle().radius(3).attr({ cx: position.x - canvasOffset.x - containerOffset.x, cy: position.y - canvasOffset.y - containerOffset.y}).addClass('labelpoint');
+    var point = container.parent().circle().radius(3).attr({
+        cx: position.x - canvasOffset.x - containerOffset.x,
+        cy: position.y - canvasOffset.y - containerOffset.y
+    }).addClass('labelpoint');
     point.draggable();
     return point;
 }
 var imgSelected = "";
 var selectedElements = [];
-var selectedTool = null, selectedElement = null;
+var selectedTool = null,
+    selectedElement = null;
 var alreadyDrawing = false;
 
 var plugins = {
-    "facepp" : {
+    "facepp": {
         title: "Face Plus Plus",
         tagName: 'facepp'
     }
 }
 var pluginsStore = {
-    "facepp" : {
-    }
+    "facepp": {}
 }
