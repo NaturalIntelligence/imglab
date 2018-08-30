6c8116e3710eaa7beb06e86eeae4a65cfacd9a0d
Alvin Voo <xoalvinox@gmail.com>
2018-08-30 12:44:40 +0800

    feat: added zoomStepSize to adjust zoom sensitivity

--- js/settings.js
+++ js/settings.js
@@ -3,7 +3,9 @@ var appConfig = {
         syncingInterval : 10 * 1000,  //10 seconds
         enable : true,
         deleteIfExported: true, //Mark the data as saved when exported as nimn format, and delte the copy from browser cache.
-    }
+    },
+    featurePointSize: 3,
+    zoomStepSize: 0.1
 };
 function displaySettingsModal(){
     $.dialog({
