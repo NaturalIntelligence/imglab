cd33d6aace760dc7f98aa58a988079eb39cc6a02
alextychan <alextychan@gmail.com>
2018-10-19 15:34:55 +0800

    Fixed bug - Multi-select now reflects on canvas and side panel (if active)

--- src/components/label-panel/panel/list-feature-point.vue
+++ src/components/label-panel/panel/list-feature-point.vue
@@ -8,7 +8,9 @@
       </small>
     </p>
 
-    <div id="feature-points-list">
+    <div
+      id="feature-points-list"
+    >
       <ul id="fpoints-list">
         <li
           class="li-fpoints grey-border">
@@ -25,14 +27,14 @@
                 :class="{ focusInput : selectedFeaturePoints.includes(featurePoint.id) }"
                 :ref="featurePoint.id"
                 :value="featurePoint.label"
-                @click="onClick($event, featurePoint)"
+                @click.stop="onClick($event, featurePoint)"
                 @change="setFeaturePointLabel($event, featurePoint)"
                 @keyup.enter="setFeaturePointLabel($event, featurePoint)"
               >
               <font-awesome-icon
                 :icon="['far', 'trash-alt']"
                 style="font-size: 1.5em"
-                @click="deleteFeaturePoint(featurePoint.id)"
+                @click.stop="deleteFeaturePoint(featurePoint.id)"
               >
               </font-awesome-icon>
             </div>
@@ -47,6 +49,7 @@
 import { mapGetters, mapMutations } from "vuex";
 import draggable from "vuedraggable";
 import { removeFeaturePoint } from "../../../utils/actions";
+import { _ } from "../../../utils/app";
 
 export default {
   components: {
@@ -108,12 +111,24 @@ export default {
       });
     },
 
-    deselectAll() {
+    /**
+     * Deselect shape on canvas, does not remove shape from store
+     */
+    deselectShape() {
+      let svgShape = this.$svg.get(this.selectedShape);
+      svgShape.selectize(false);
+    },
+
+    /**
+     * Deselect and remove all feature points in selected shape
+     */
+    deselectShapeFeaturePoints() {
       this.selectedFeaturePoints.forEach(featurePointID => {
         let svgFP = this.$svg.get(featurePointID);
         svgFP.selectize(false);
       })
 
+      // Remove feature points from store
       this.setSelectedElements({ shapes: this.selectedShape });
     },
 
@@ -123,10 +138,8 @@ export default {
      * @param {FeaturePoint} featurePoint - featurePoint clicked
      */
     onClick(event, featurePoint) {
-      if (!event.ctrlKey) {
-        this.deselectAll();
-      }
-
+      if (!event.ctrlKey) this.deselectShapeFeaturePoints();
+      this.deselectShape();
       this.addSelectedElement({ featurePointID: featurePoint.id });
     },
 
@@ -142,6 +155,12 @@ export default {
         newLabel: event.target.value
       })
     }
+  },
+  mounted() {
+    _.on(document, "click", this.deselectShapeFeaturePoints);
+  },
+  destroyed() {
+    _.off(document, "click", this.deselectShapeFeaturePoints);
   }
 }
 </script>
