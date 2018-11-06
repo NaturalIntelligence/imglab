e595e8fd0f5f169caba8140f67eab269f9c18b55
Joe B <joebanegas@gmail.com>
2018-11-05 23:24:02 -0600

    no longer calls twice

--- js/common-actions.js
+++ js/common-actions.js
@@ -2,14 +2,15 @@
 
     if (e.key == '≠') {//Zoom in
         $("#tool-zoom").click();
-        $("[data-zoom-type='in']").zoom();
+        $("[data-zoom-type='in']").click();
+        console.log('test');
         e.preventDefault();
         e.stopPropagation();
      }
     
      if (e.key == '–') {//Zoom out
         $("#tool-zoom").click();
-        $("[data-zoom-type='out']").zoom();
+        $("[data-zoom-type='out']").click();
         e.preventDefault();
         e.stopPropagation();
      }
