918b83277da6c21ef1ed5a8143b4ef808f3b6626
kozlovvski <mcbiggos@gmail.com>
2019-03-26 20:40:57 +0100

    Added url to base64 parsing by XMLHttpRequest

--- js/urlUpload.js
+++ js/urlUpload.js
@@ -1,3 +1,4 @@
+// shows a popup box when $(.upload-from-url-button) is clicked
 const displayURLUploader = () => {
     $.dialog({
         title : '<i class="icon-link" style="color: #138496; font-size: 1.2em;"></i>Upload image from URL',
@@ -10,4 +11,25 @@ const displayURLUploader = () => {
             riot.mount('url-uploader-window');
         }
     })
+}
+
+// sends a HTTP Request with pasted link and converts image to base64
+const uploadFileFromURL = (url) => {
+    console.log(url);
+
+    const xmlHTTP = new XMLHttpRequest();
+    xmlHTTP.open('GET', url, true);
+    xmlHTTP.responseType = 'arraybuffer';
+
+    xmlHTTP.onload = function() {
+        const arr = new Uint8Array(this.response);
+        const raw = String.fromCharCode(...arr);
+        const b64 = btoa(raw);
+        const dataURL = `data:image/png;base64,${b64}`;
+        console.log(dataURL);
+
+        // here goes code that uses the dataURL
+    };
+    
+    xmlHTTP.send();
 }
