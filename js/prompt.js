a09086cf8b91413e472ecc2054edd3dc9706695f
Amit K Gupta <amitguptagwl@users.noreply.github.com>
2023-03-26 06:41:15 +0530

    Update prompt.js

--- js/prompt.js
+++ js/prompt.js
@@ -86,15 +86,9 @@ function displayDonationPrompt() {
   $.dialog({
     title: "Donate",
     content: `<div style="text-align:center;">
-                <div>
-                    <div onclick="javascript:tezpayment()" class="chip" style="cursor: pointer;">
-                        <img src="img/tez_logo2.png" alt="Tez" width="95" height="95"> Tez
-                    </div>
-                </div>
-                <br>
-                <div><a onclick="javascript:logPaypal()" href="https://www.paypal.me/amitkumarguptagwl" target="_blank"><img src="img/support_paypal.svg" width="200px"></a></div>
-                <br>
-                <div><a onclick=""javascript:logPateron()" href="https://www.patreon.com/bePatron?u=9531404"  target="_blank"><img src="img/support_patreon.svg" width="200px"></a></div>
+
+                <div><a onclick="javascript:logPaypal()" href="https://paypal.me/naturalintelligence" target="_blank"><img src="img/support_paypal.svg" width="200px"></a></div>
+                
             <div>`,
     escapeKey: true,
     backgroundDismiss: true,
